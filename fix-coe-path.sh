411a739f323eff426acff4527c51f289f1908117
joao-o <joao.s.oliveira@tecnico.ulisboa.pt>
2024-04-22 19:00:05 +0100

    fixed coe file relative path issue in project.tcl: use a text substitution pre-commit hook to fix it everytime

---
+++ fix-coe-path.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#This should fix the coe file path in the generated path 
+#this is best run as a pre commit hook
+#run the script to install the hook
+
+cd .git/hooks
+if [ ! -f pre-commit ]; then
+    cat << 'EOF' > pre-commit
+#!/bin/sh
+
+sed -i -e 's|coefficient_file.*/src|coefficent_file [pwd]{/src|' project.tcl
+
+EOF
+chmod +x pre-commit
+fi
